08645f5122d6e07b0988472a0a654d3dfd36f619
Lucas DA COSTA <Ldacosta7797@gmail.com>
2026-03-11 11:02:20 +0100

    feat: navbar et routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProduitsComponent } from './pages/produits/produits.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'produits', pathMatch: 'full' },
+  { path: 'produits', component: ProduitsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
