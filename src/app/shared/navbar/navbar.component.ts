2fc779555059894bca8162c81ec3b829c054d7d9
Walid ESSIOUI <walid.essioui@koesio.com>
2026-03-18 15:27:29 +0100

    Update Navbar design

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
-import { AuthService } from '../../core/services/auth.service';
+import { AuthService } from '../../core/services/auth.service'; // adapte le chemin
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +12,18 @@ import { AuthService } from '../../core/services/auth.service';
 })
 export class NavbarComponent {
 
-  menuOpen = false;
+  menuOpen   = false;
+  isScrolled = false;
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) {}
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    this.isScrolled = window.scrollY > 8;
+  }
 
   toggleMenu(): void { this.menuOpen = !this.menuOpen; }
   closeMenu(): void  { this.menuOpen = false; }
